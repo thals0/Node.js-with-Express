c08571fa8c40acdd57a9298866321778ba63ba0f
SoMin <thals0107@naver.com>
2022-09-14 18:17:13 +0900

    board service 완료
    callback 방식에서 async awiat 방식으로 변경

--- routes/board.js
+++ routes/board.js
@@ -6,17 +6,13 @@ const router = express.Router();
 
 const mongoClient = require('./mongo');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // 글 전체 목록 보여주기
-  MongoClient.connect(uri, (err, db) => {
-    const data = db.db('node1').collection('board');
-
-    data.find({}).toArray((err, result) => {
-      const ARTICLE = result;
-      const articleLen = ARTICLE.length;
-      res.render('board', { ARTICLE, articleCounts: articleLen });
-    });
-  });
+  const client = await mongoClient.connect();
+  const cursor = client.db('node1').collection('board');
+  const ARTICLE = await cursor.find({}).toArray();
+  const articleLen = ARTICLE.length;
+  res.render('board', { ARTICLE, articleCounts: articleLen });
 });
 
 router.get('/write', (req, res) => {
@@ -24,24 +20,17 @@ router.get('/write', (req, res) => {
   res.render('board_write');
 });
 
-router.post('/write', (req, res) => {
+router.post('/write', async (req, res) => {
   // 글 추가 기능 수행
   if (req.body.title && req.body.content) {
     const newArticle = {
       title: req.body.title,
       content: req.body.content,
     };
-    MongoClient.connect(uri, (err, db) => {
-      const data = db.db('node1').collection('board');
-
-      data.insertOne(newArticle, (err, result) => {
-        if (err) {
-          throw err;
-        } else {
-          res.redirect('/board');
-        }
-      });
-    });
+    const client = await mongoClient.connect();
+    const cursor = client.db('node1').collection('board');
+    await cursor.insertOne(newArticle);
+    res.redirect('/board');
   } else {
     const err = new Error('데이터가 없습니다.');
     err.statusCode = 404;
@@ -49,38 +38,24 @@ router.post('/write', (req, res) => {
   }
 });
 
-router.get('/edit/title/:title', (req, res) => {
+router.get('/edit/title/:title', async (req, res) => {
   // 글 수정 모드로 이동
-  MongoClient.connect(uri, (err, db) => {
-    const data = db.db('node1').collection('board');
-    data.findOne({ title: req.params.title }, (err, result) => {
-      if (err) {
-        throw err;
-      } else {
-        const selectedArticle = result;
-        res.render('board_edit', { selectedArticle });
-      }
-    });
-  });
+  const client = await mongoClient.connect();
+  const cursor = client.db('node1').collection('board');
+  const selectedArticle = await cursor.findOne({ title: req.params.title });
+  res.render('board_edit', { selectedArticle });
 });
 
-router.post('/edit/title/:title', (req, res) => {
+router.post('/edit/title/:title', async (req, res) => {
   // 글 수정 기능 수행
   if (req.body.title && req.body.content) {
-    MongoClient.connect(uri, (err, db) => {
-      const data = db.db('node1').collection('board');
-      data.updateOne(
-        { title: req.params.title },
-        { $set: { title: req.body.title, content: req.body.content } },
-        (err, result) => {
-          if (err) {
-            throw err;
-          } else {
-            res.redirect('/board');
-          }
-        }
-      );
-    });
+    const client = await mongoClient.connect();
+    const cursor = client.db('node1').collection('board');
+    await cursor.updateOne(
+      { title: req.params.title },
+      { $set: { title: req.body.title, content: req.body.content } }
+    );
+    res.redirect('/board');
   } else {
     const err = new Error('요청 값이 없습니다.');
     err.statusCode = 404;
@@ -88,19 +63,20 @@ router.post('/edit/title/:title', (req, res) => {
   }
 });
 
-router.delete('/delete/title/:title', (req, res) => {
+router.delete('/delete/title/:title', async (req, res) => {
   // 글 삭제 기능 수행
-  MongoClient.connect(uri, (err, db) => {
-    const data = db.db('node1').collection('board');
-    data.deleteOne({ title: req.params.title }, (err, result) => {
-      if (err) {
-        res.end('해당 데이터가 없습니다.');
-        throw err;
-      } else {
-        res.send('삭제 완료');
-      }
-    });
-  });
+  const client = await mongoClient.connect();
+  const cursor = client.db('node1').collection('board');
+  const result = await cursor.deleteOne({ title: req.params.title });
+
+  // acknowledged: T or F 반환
+  if (result.acknowledged) {
+    res.send('삭제 완료');
+  } else {
+    const err = new Error('삭제 실패');
+    err.statusCode = 404;
+    throw err;
+  }
 });
 
 module.exports = router;
