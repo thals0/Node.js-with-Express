438de8161e758ed07763fd1d181ec58ba2923ea0
SoMin <thals0107@naver.com>
2022-09-16 18:46:18 +0900

    session으로 login 구현

---
+++ routes/login session.js
@@ -0,0 +1,44 @@
+// @ts-check
+
+const express = require('express');
+
+const router = express.Router();
+const mongoClient = require('./mongo');
+
+router.get('/', (req, res) => {
+  res.render('login');
+});
+
+router.post('/', async (req, res) => {
+  const client = await mongoClient.connect();
+  const userCursor = client.db('node1').collection('users');
+  const idResult = await userCursor.findOne({ id: req.body.id });
+
+  if (idResult !== null) {
+    if (idResult.password === req.body.password) {
+      req.session.login = true;
+      req.session.userId = req.body.id;
+      res.redirect('/board');
+    } else {
+      // 비밀번호가 틀림
+      res.status(300);
+      res.send(
+        '비밀번호가 틀렸습니다. <br><a href="/login">로그인 페이지로 이동</a>'
+      );
+    }
+  } else {
+    res.status(300);
+    res.send(
+      '해당 id가 없습니다. <br><a href="/login">로그인 페이지로 이동</a>'
+    );
+  }
+});
+
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) throw err;
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
